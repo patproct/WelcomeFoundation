e81ca8e168dc9d6fe4cdcffb2e0fd84eb8c8af73
Patrick Proctor <patproct@Patricks-MacBook-Pro.local>
2012-03-13 18:13:48 -0400

    Initial Commit

---
+++ WelcomeFoundation/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WelcomeFoundation
+//
+//  Created by Patrick Proctor on 3/13/12.
+//  Copyright (c) 2012 Soylent Industries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
