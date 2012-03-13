ce96de8144161af32921eae1700e93a6ffc0d9af
Patrick Proctor <patproct@Patricks-MacBook-Pro.local>
2012-03-13 18:15:16 -0400

    Added Greeting class

---
+++ WelcomeFoundation/Greeting.h
@@ -0,0 +1,13 @@
+//
+//  Greeting.h
+//  WelcomeFoundation
+//
+//  Created by Patrick Proctor on 3/13/12.
+//  Copyright (c) 2012 Soylent Industries. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Greeting : NSObject
+
+@end
---
+++ WelcomeFoundation/Greeting.m
@@ -0,0 +1,13 @@
+//
+//  Greeting.m
+//  WelcomeFoundation
+//
+//  Created by Patrick Proctor on 3/13/12.
+//  Copyright (c) 2012 Soylent Industries. All rights reserved.
+//
+
+#import "Greeting.h"
+
+@implementation Greeting
+
+@end
